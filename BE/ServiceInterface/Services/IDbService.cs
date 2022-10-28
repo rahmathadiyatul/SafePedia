688726ef21d11cd6e1c7f9d7cb1a13642ee54d49
Rahmat Hadiyatul Akbar <rahmathadiyatulakbar@gmail.com>
2022-10-28 20:25:12 +0700

    Add files via upload

---
+++ BE/ServiceInterface/Services/IDbService.cs
@@ -0,0 +1,16 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceInterface.Services
+{
+    public interface IDbService
+    {
+        public Task<List<Karyawan>> GetKaryawanData(string command, object param);
+        public Task<List<Kendaraan>> GetKendaraanData(string command, object param);
+
+    }
+}
---
+++ BE/ServiceInterface/Services/IKendaraanService.cs
@@ -0,0 +1,14 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceInterface.Services
+{
+    public interface IKendaraanService
+    {
+        public Task<List<Kendaraan>> GetKendaraan();
+    }
+}
