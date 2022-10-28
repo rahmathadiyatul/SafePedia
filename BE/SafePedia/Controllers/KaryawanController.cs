f88d7a09a2b850158f7b48ce19d3f07edaa6aaf3
Rahmat Hadiyatul Akbar <rahmathadiyatulakbar@gmail.com>
2022-10-28 20:27:43 +0700

    Add files via upload

---
+++ BE/SafePedia/Controllers/KaryawanController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceInterface.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SafePedia.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KaryawanController : Controller
+    {
+        private readonly IKaryawanService karyawanService;
+        public KaryawanController(IKaryawanService karyawanService)
+        {
+            this.karyawanService = karyawanService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var result = await karyawanService.GetKaryawan();
+            return Ok(result);
+        }
+    }    
+}
---
+++ BE/SafePedia/Controllers/KendaraanController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceInterface.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SafePedia.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KendaraanController : Controller
+    {
+        private readonly IKendaraanService kendaraanService;
+        public KendaraanController(IKendaraanService kendaraanService)
+        {
+            this.kendaraanService = kendaraanService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var result = await kendaraanService.GetKendaraan();
+            return Ok(result);
+        }
+    }
+}
