0dbd4bef5bd9030f4e1483933c705f8ccec8c934
Rahmat Hadiyatul Akbar <rahmathadiyatulakbar@gmail.com>
2022-10-28 20:17:32 +0700

    Add files via upload

---
+++ BE/Data/Repositories/KendaraanRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataInterface.Repositories;
+using Model.Entities;
+using ServiceInterface.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class KendaraanRepository : IKendaraanRepository
+    {
+        private readonly IDbService _dbService;
+        public KendaraanRepository(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        public async Task<List<Kendaraan>> GetDataKendaraan()
+        {
+            List<Kendaraan> result = await _dbService.GetKendaraanData(
+                "SELECT * FROM KendaraanPerusahaan;", new { });
+            return result;
+        }
+    }
+}
